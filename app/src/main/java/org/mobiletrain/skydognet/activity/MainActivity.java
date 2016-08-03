1acb599cd91e65e6891f34123f80a8d9cdb2790a
hajiangwu <1217210195@qq.com>
2016-08-03 09:58:31 +0800

    second

--- app/src/main/java/org/mobiletrain/skydognet/activity/MainActivity.java
+++ app/src/main/java/org/mobiletrain/skydognet/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package org.mobiletrain.skydognet;
+package org.mobiletrain.skydognet.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.mobiletrain.skydognet.R;
 
 public class MainActivity extends AppCompatActivity {
 
